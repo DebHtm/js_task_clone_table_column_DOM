322b7c5120d6924eeb72fb362791f7e841a279d6
Denys <yolm9yom@gmail.com>
2025-08-24 16:50:17 +0300

    DOM6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const globalTr = document.querySelectorAll('table tr');
+
+function addColumn(allTr) {
+  for (let k = 0; k < allTr.length; k++) {
+    const row = allTr[k];
+    const copy = row.cells[1];
+    const last = row.cells[row.cells.length - 1];
+    const clone = copy.cloneNode(true);
+
+    row.insertBefore(clone, last);
+  }
+}
+
+addColumn(globalTr);
