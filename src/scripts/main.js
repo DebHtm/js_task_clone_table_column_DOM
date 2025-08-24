928eb7e24601aed3d55258a622e8046e99a5306e
Denys <yolm9yom@gmail.com>
2025-08-24 17:05:39 +0300

    DOM6.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,29 @@
 'use strict';
 
-const globalTr = document.querySelectorAll('table tr');
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+const tbody = document.querySelector('tbody');
 
-function addColumn(allTr) {
-  for (let k = 0; k < allTr.length; k++) {
-    const row = allTr[k];
-    const copy = row.cells[1];
-    const last = row.cells[row.cells.length - 1];
-    const clone = copy.cloneNode(true);
+function addColumn() {
+  for (let i = 0; i < arguments.length; i++) {
+    const rows = arguments[i].rows;
 
-    row.insertBefore(clone, last);
+    for (let k = 0; k < rows.length; k++) {
+      const row = rows[k];
+
+      if (row.cells.length < 2) {
+        continue;
+      }
+
+      const copy = row.cells[1];
+      const last = row.cells[row.cells.length - 1];
+      const clone = copy.cloneNode(true);
+
+      row.insertBefore(clone, last);
+    }
   }
 }
 
-addColumn(globalTr);
+document.addEventListener('DOMContentLoaded', () => {
+  addColumn(thead, tfoot, tbody);
+});
